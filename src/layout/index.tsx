d4d8305d804a12dc0091098b55775790d57c9b5b
Minsu Kim <alstn2468_@naver.com>
2021-01-07 19:18:46 +0900

    Noto Sans Kr 폰트 추가

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,6 +3,7 @@ import { Global, css } from '@emotion/react';
 import { Helmet } from 'react-helmet-async';
 import { ThemeProvider, defaultTheme } from '~/src/components/themeContext';
 import { l10nContext } from '~/src/components/l10nContext';
+import NotoSansKrFont from '~/src/components/NotoSansKrFont';
 
 type LayoutProps = {
   l10n: React.ContextType<typeof l10nContext>;
@@ -23,11 +24,22 @@ const Layout: React.FC<LayoutProps> = ({
         )}
         <Global
           styles={css({
-            body: {
+            '*': {
+              fontFamily: 'inherit',
+            },
+            'body': {
               margin: 0,
+              textRendering: 'optimizeLegibility',
+              wordBreak: 'break-word',
+              fontFamily: '"Noto Sans KR__subset", "Noto Sans KR", sans-serif',
+              WebkitFontSmoothing: 'antialiased',
+            },
+            'body:lang(ko)': {
+              wordBreak: 'keep-all',
             },
           })}
         />
+        <NotoSansKrFont />
         {children}
       </l10nContext.Provider>
     </ThemeProvider>
