10c432b81538d921e68c41c8d7bfe417624f33e4
Minsu Kim <alstn2468_@naver.com>
2021-01-10 20:22:56 +0900

    GatsbySeo 사용해해서 오픈그래프, 메타데이터  설정

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Global, css } from '@emotion/react';
 import { Helmet } from 'react-helmet-async';
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
 import { ThemeProvider, defaultTheme } from '~/src/components/themeContext';
 import { l10nContext } from '~/src/components/l10nContext';
+import { useSiteMetadata } from '~/src/utils';
 import NotoSansKrFont from '~/src/components/NotoSansKrFont';
 
 type LayoutProps = {
@@ -13,14 +15,37 @@ const Layout: React.FC<LayoutProps> = ({
   l10n,
   children,
 }) => {
+  const siteMetadata = useSiteMetadata();
   const currentLanguage = l10n?.language;
   return (
     <ThemeProvider theme={defaultTheme}>
       <l10nContext.Provider value={l10n}>
         {currentLanguage && (
-          <Helmet>
-            <html lang={currentLanguage} />
-          </Helmet>
+          <>
+            <Helmet>
+              <html lang={currentLanguage} />
+            </Helmet>
+            <GatsbySeo
+              title={'Gatsby Resume'}
+              description={'Static website resume with GatsbyJS, TypeScript'}
+              canonical={siteMetadata.siteUrl + `/${currentLanguage}`}
+              openGraph={{
+                type: 'website',
+                url: siteMetadata.siteUrl + `/${currentLanguage}`,
+                title: 'Gatsby Resume',
+                description: 'Static website resume with GatsbyJS, TypeScript',
+                images: [
+                  {
+                    url: `${siteMetadata.siteUrl}/og.jpeg`,
+                    width: 1200,
+                    height: 630,
+                    alt: 'Gatsby Resume',
+                  }
+                ],
+                site_name: 'Gatsby Resume',
+              }}
+            />
+          </>
         )}
         <Global
           styles={css({
