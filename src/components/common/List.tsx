9f89c943039b7603348c6d755f4c8efa3c3e7fbe
Minsu Kim <alstn2468_@naver.com>
2021-01-10 18:47:32 +0900

    List 컴포넌트 ul 태그로 변경

--- src/components/common/List.tsx
+++ src/components/common/List.tsx
@@ -1,7 +1,7 @@
 import { rem } from 'polished';
 import { styled } from '~/src/components/themeContext';
 
-const List = styled.dl((props) => ({
+const List = styled.ul((props) => ({
   padding: 0,
   [props.theme.media['md']]: {
     padding: `0 ${rem(16)}`,
